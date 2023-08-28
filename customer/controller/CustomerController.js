20eee863202dc21d643b9a49ae95a06623cc885c
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-08-29 03:18:12 +0700

    Fix UI

--- customer/controller/CustomerController.js
+++ customer/controller/CustomerController.js
@@ -6,14 +6,19 @@ export let renderProduct = (product) => {
     contentHTML +=
       /*html*/
       `
-    <div class="card col-3" style="width: 18rem;">
-    <img class="card-img-top" src="../assets/img/neon.jpg" alt="Card image cap">
-    <div class="card-body">
-    <h5 class="card-title">${name} </h5>
-    <p class= "card-text" >${desc}</p>
-    <a href="#" class="btn btn-primary">Add to cart</a>
-  </div>
-</div>`;
+      <div class="col-3">
+      <div class="card">
+      <img class="card-img-top" src="../assets/img/iphone1.jpg" alt="Card image cap">
+      <div class="card-body">
+      <h5 class="card-title">${name} </h5>
+      <p class= "card-text" >${desc}</p>
+      </div>
+      <a href="#" class="add-to-card btn btn-primary">Add to cart</a>
+
+      </div>
+      </div>
+   
+`;
   });
   result.innerHTML = contentHTML;
 };
