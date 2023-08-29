37ee313454944d73b392865baa49678708812950
Hoàng Các Loan Châu <hoangchau741@gmail.com>
2023-08-29 19:17:19 +0700

    fix UI

--- customer/controller/CustomerController.js
+++ customer/controller/CustomerController.js
@@ -1,21 +1,22 @@
 export let renderProduct = (product) => {
   let result = document.getElementById("phone-list");
   let contentHTML = "";
-  product.forEach((item) => {
+  product.reverse().forEach((item) => {
     let { id, name, backCamera, desc, frontCamera, img, price, screen, type } = item;
     contentHTML +=
       /*html*/
       `
       <div class="col-3">
-      <div class="card">
+      <div class="card h-100">
       <img class="card-img-top" src="../assets/img/iphone1.jpg" alt="Card image cap">
       <div class="card-body">
       <h5 class="card-title">${name} </h5>
-      <p class= "card-text" >${desc}</p>
+      <p class= "card-text" ><span class="font-weight-bold desc ">Descripton: </span>${desc}</p>
+      <p class= "card-text" ><span class="font-weight-bold desc">Price: </span>${price}</p>
+
       </div>
       <div class="cart-button">
-      <a href="#" class="add-to-card btn btn-primary">Add to cart</a>
-      <i class="fa fa-cart-arrow-down"></i>
+      <a href="#" class="add-to-card btn btn-primary" ><span>Add to cart </span><i class="fa fa-cart-arrow-down"></i></a>
       </div>
 
       </div>
